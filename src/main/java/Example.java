aaefb8be384873cbc32ac5cc84167f71280bc899
JeongHoonKim <devkjh@gmail.com>
2018-01-12 13:19:54 +0900

    add name

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Example {
         @RequestMapping("/")
         String home() {
-                return "Hello World!";
+                return "jeonghoonkim Hello World!";
         }
         public static void main(String[] args) throws Exception {
                 SpringApplication.run(Example.class, args);
